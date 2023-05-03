a3a07125d6527353120eeea4fd6d9d3aad3b8a06
Logon Aluno <logonrmlocal@fiap.com.br>
2023-05-02 22:44:36 -0300

    first commit

---
+++ crud.js
@@ -0,0 +1,10 @@
+document.getElementById('modal-click').addEventListener(
+    'click',
+    () => {
+        let titulo = document.getElementById('titulo-input').value
+        let descricao = document.getElementById('descricao-input').value
+        let pontos = document.getElementById('pontos-input').value
+        let categorias = document.getElementById('categorias-input').value
+        console.log(titulo)
+    }
+)
